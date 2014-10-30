6234dba22d13ed23b4e66a23118528011f2d37e4
Jennie Chamberlin <chamberlin.jennie@gmail.com>
2014-10-30 15:50:32 -0500

    clean up and refactor of javascript

--- application.js
+++ application.js
@@ -1,24 +1,5 @@
 $(document).ready(function(){
 
-	var User = function(userInfo) {
-		this.email = userInfo.email;
-		this.apiToken = userInfo.api_token;
-		this.id = userInfo.id;
-		this.todos = userInfo.todos;
-	}
-
-	var getTodos = function(userId, apiToken){
-		var request = $.ajax({
-			url: "http://recruiting-api.nextcapital.com/users/" + userId + "/todos.json?api_token=" + apiToken,
-			type: "GET",
-			success: function(todos) {
-				console.log("i just got some todos!")
-				console.log(todos)
-			}
-			
-		});
-	};
-	
 	$("#login").click(function(event) {
 		event.preventDefault();
 
@@ -27,55 +8,100 @@ $(document).ready(function(){
 		var request = $.ajax({
 			url: "http://recruiting-api.nextcapital.com/users/sign_in",
 			type: "POST",
+			dataType: 'json',
 			data:  { email: email, password: password },
 			success: function(data) {
-				var user = createUser(data);
-				if (user.apiToken != null) {
-					$('#sign-in-form').hide();
-				listTodos(user);
-				showTodos();
-				}
+				setSessionInfo(data);
+				$('#sign-in-form').find("input[type=text], input[type=password]").val("");
+				hideSignInForm();
 			}
 		})
-
-		var createUser = function(userInfo) {
-			return new User(userInfo);
-		}
-
-
-		var listTodos = function(user) {
-			console.log(user)
-			console.log(user.todos)
-			if (user.todos.length === 0) {
-				console.log("wassup")
-				$('#todo-list').prepend("<p>looks like you need to add some todos!</p>")
-				}
-			else {
-				console.log("what is this doing?!?!!?")
-				$('#todo-list').append("<p>lalalalalal!</p>")
-				}	
-			};
-
-			var showTodos = function() {
-				$('#todo-list').show()
-			}
-			
+		request.success(showTodos());
+		request.success(getTodos());
 
 	})
+
+	var showTodos = function() {
+		$("#todo-space").show();
+	}
+
+	var getTodos = function(){
+		var request = $.ajax({
+			url: "http://recruiting-api.nextcapital.com/users/" + sessionStorage.userId + "/todos.json?api_token=" + sessionStorage.apiToken,
+			type: "GET"
+		});
+		request.success(listTodos);
+	};
+	
+
+	var listTodos = function(data) {
+		if (data.length === 0) {
+			$("#todo-list").prepend("<p>looks like you need to add some todos!</p>")
+			}
+		else {
+			for (i = 0; i < data.length; i++) {
+				$('#todo-list').append("<p>" + data[i].description + "</p>")
+			}
+		}	
+	};
+
+
+	$('#new-todo-button').click(function(event){
+		event.preventDefault();
+		var description = document.getElementById("new-todo").value;
+		$('#new-todo-form').find("input[type=text]").val("");
+		var request = $.ajax({
+			url: "http://recruiting-api.nextcapital.com/users/" + sessionStorage.userId + "/todos",
+			type: "POST",
+			data:  { api_token: sessionStorage.apiToken, todo: { description:  description } }
+		});
+
+		request.success(appendTodo);
+
+	})
+	
+	var setSessionInfo = function(data) {
+		sessionStorage.setItem("apiToken", data.api_token);
+		sessionStorage.setItem("userId", data.id);
+	}
+
+	var hideSignInForm = function() {
+		if (sessionStorage.apiToken != null) {
+			$('#sign-in-form').hide();
+			$("#logout").show();
+		}
+	}
+
+	var appendTodo = function(todo) {
+		$('#todo-list').append("<p>" + todo.description + "</p>")
+	}
+
+	$("#logout").click(function(event){
+		event.preventDefault();
+		
+		sessionStorage.clear();
+		$('#todo-list').empty();
+		$('#todo-space').hide();
+		$('#sign-in-form').show();
+
+	})
+
+			
+
 	
 });
 
 
 
 
-(function(){
-	var app = angular.module('todo', []);
+// (function(){
+// 	var app = angular.module('todo', []);
 
-	app.controller('TodoController', function(){
-		this.list = getTodos();
+// 	app.controller('TodoController', function(){
+// 		this.list = getTodos();
 
-	});
+// 	});
 
 
 
-})();
+// })();
