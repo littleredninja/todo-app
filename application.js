795e6adc1622ee600bd868a2f5ac2a28ff27a786
Jennie Chamberlin <chamberlin.jennie@gmail.com>
2014-11-05 18:37:16 -0600

    added sign up form functionality, refactor

--- application.js
+++ application.js
@@ -4,14 +4,14 @@ $(document).ready(function(){
 		this.id = data.id;
 		this.description = data.description;
 		this.isComplete = data.is_complete;
-	}
+	};
 
 	$("#login-button").click(function(event) {
 		event.preventDefault();
 
-		var email = document.getElementById("email").value;
-		var password = document.getElementById("password").value;
-		var request = $.ajax({
+		var email = document.getElementById("login-email").value, 
+		password = document.getElementById("login-password").value,
+		request = $.ajax({
 			url: "http://recruiting-api.nextcapital.com/users/sign_in",
 			type: "POST",
 			dataType: 'json',
@@ -24,19 +24,55 @@ $(document).ready(function(){
 				getTodos();
 			},
 			error: function(data) {
-				alert("oops! something went wrong, please try again");
+				alert("Oops! Something went wrong, so please try again.");
 				clearInputForms();
 			}
-		})
-	})
+		});
+	});
+
+	$("#sign-up-button").click(function(event) {
+		event.preventDefault();
+
+		var email = document.getElementById("sign-up-email").value, 
+		password = document.getElementById("sign-up-password").value,
+		request = $.ajax({
+			url: "http://recruiting-api.nextcapital.com/users",
+			type: "POST",
+			dataType: 'json',
+			data:  { email: email, password: password },
+			success: function(data) {
+				setSessionInfo(data);
+				clearInputForms();
+				hideLogIn();
+				showTodos();
+				getTodos();
+			},
+			error: function(data) {
+				alert("Oops! Something went wrong, so please try again.");
+				clearInputForms();
+			}
+		});
+	});
+
+	var setSessionInfo = function(data) {
+		sessionStorage.setItem("apiToken", data.api_token);
+		sessionStorage.setItem("userId", data.id);
+	};
 
 	var clearInputForms = function() {
 		$("input[type=text], input[type=password]").val("");
-	}
+	};
+
+	var hideLogIn = function() {
+		if (sessionStorage.apiToken !== null) {
+			$('#login-space').hide();
+			$("#logout").show();
+		}
+	};
 
 	var showTodos = function() {
 		$("#todo-space").show();
-	}
+	};
 
 	var getTodos = function(){
 		var request = $.ajax({
@@ -47,18 +83,13 @@ $(document).ready(function(){
 	};
 	
 	var listTodos = function(data) {
-		if (data.length === 0) {
-			$("#todo-list").prepend("<p>Looks like you need to add some todos!</p>")
-		}
-		else {
-			for (i = 0; i < data.length; i++) {
-				var todo = new Todo(data[i]);
-				if (todo.isComplete === false) {
-					appendTodo(todo);
-				}
-				else {
-					$('#todo-done').append("<li class='todo' draggable='true' id='" + todo.id + "'>" + todo.description + "</li>");
-				}
+		for (i = 0; i < data.length; i++) {
+			var todo = new Todo(data[i]);
+			if (todo.isComplete === false) {
+				appendTodo(todo);
+			}
+			else {
+				$('#todo-done').append("<li class='todo' draggable='true' id='" + todo.id + "'>" + todo.description + "</li>");
 			}
 		}
 	};
@@ -71,9 +102,9 @@ $(document).ready(function(){
 
   $( "#todo-done" ).droppable({
     drop: function( event, ui ) {
-      var todoId = $(ui.draggable).attr("id");
-      var todoDescription = $(ui.draggable).text();
-			var request = $.ajax({
+      var todoId = $(ui.draggable).attr("id"),
+      todoDescription = $(ui.draggable).text(),
+			request = $.ajax({
 				url: "http://recruiting-api.nextcapital.com/users/" + sessionStorage.userId + "/todos/" + todoId,
 				type: "PUT",
 				data:  { api_token: sessionStorage.apiToken, todo: {description: todoDescription, is_complete: true }},
@@ -83,9 +114,9 @@ $(document).ready(function(){
 
   $( "#todo-list" ).droppable({
     drop: function( event, ui ) {
-      var todoId = $(ui.draggable).attr("id");
-      var todoDescription = $(ui.draggable).text();
-			var request = $.ajax({
+      var todoId = $(ui.draggable).attr("id"),
+      todoDescription = $(ui.draggable).text(),
+			request = $.ajax({
 				url: "http://recruiting-api.nextcapital.com/users/" + sessionStorage.userId + "/todos/" + todoId,
 				type: "PUT",
 				data:  { api_token: sessionStorage.apiToken, todo: {description: todoDescription, is_complete: false }},
@@ -103,23 +134,11 @@ $(document).ready(function(){
 		});
 		clearInputForms();
 		request.success(appendTodo);
-	})
+	});
 	
-	var setSessionInfo = function(data) {
-		sessionStorage.setItem("apiToken", data.api_token);
-		sessionStorage.setItem("userId", data.id);
-	}
-
-	var hideLogIn = function() {
-		if (sessionStorage.apiToken != null) {
-			$('#login-space').hide();
-			$("#logout").show();
-		}
-	}
-
 	var appendTodo = function(todo) {
 		$('#todo-list').append("<li class='todo' draggable='true' id='" + todo.id + "'>" + todo.description + "</li>");
-	}
+	};
 
 	$("#logout").click(function(event){
 		event.preventDefault();
@@ -128,8 +147,12 @@ $(document).ready(function(){
 		$('#todo-list, #todo-done').empty();
 		$('#todo-space').hide();
 		$('#login-space').show();
+		var request = $.ajax({
+			url: "http://recruiting-api.nextcapital.com/users/sign_out",
+			type: "DELETE",
+			data:  { api_token: sessionStorage.apiToken, user_id: sessionStorage.userId }
+		});
 	});
 
 });
-
 
